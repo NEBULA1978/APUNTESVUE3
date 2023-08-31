eb4d7e3d3385421cdfb2893f7eaa1ce1f625ef34
NEBULA1978 <rpascualey@gmail.com>
2023-08-31 23:28:11 +0200

    IniciCreando componente Enlaces de atributos

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,10 @@
   <div>
     <MiComponente />
   </div>
+  <EnlacesAtributos />
 </template>
 
 <script setup>
 import MiComponente from './components/MiComponente.vue';
+import EnlacesAtributos from './components/EnlacesAtributos.vue';
 </script>
---
+++ src/components/EnlacesAtributos.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Hola {{ name }}!</h1>
+    <h2 v-bind:style="styleColor">Hola {{ name }} desde componente EnlacesAtributos</h2>
+    <!-- Forma abreviada v-bind -->
+    <!-- <h2 :style="styleColor">Hola {{ name }}</h2> -->
+</template>
+
+<script setup>
+const name = "Vue 3";
+const styleColor = "color: red;";
+</script>
