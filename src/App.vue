498624b84d95be79ee382d72ed9105957f88b547
NEBULA1978 <rpascualey@gmail.com>
2023-08-31 23:31:29 +0200

    Uso de expresiones de JavaScript

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,11 @@
     <MiComponente />
   </div>
   <EnlacesAtributos />
+  <usoExpresionJavascript />
 </template>
 
 <script setup>
 import MiComponente from './components/MiComponente.vue';
 import EnlacesAtributos from './components/EnlacesAtributos.vue';
+import usoExpresionJavascript from './components/usoExpresionJavascript.vue';
 </script>
